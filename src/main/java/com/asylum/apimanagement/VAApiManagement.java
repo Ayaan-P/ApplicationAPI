a517c8578fb07921f6ed854a925cdb90bcb4cab7
ayaan.pupala <ayaan.pupala@karsun-llc.com>
2023-03-22 13:39:53 -0400

    search, orderBy

---
+++ src/main/java/com/asylum/apimanagement/VAApiManagement.java
@@ -0,0 +1,13 @@
+package com.asylum.apimanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VAApiManagement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VAApiManagement.class, args);
+	}
+
+}
---
+++ src/test/java/com/asylum/apimanagement/ApimanagementApplicationTests.java
@@ -0,0 +1,22 @@
+package com.asylum.apimanagement;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class ApimanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void demoTest() {
+		assertTrue(true);
+	}
+
+}
